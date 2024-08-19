73853b8bf3df33abcc8cb367a229b9d79249267d
Lara Gimenez <lara.gimenez@itscordoba.edu.ar>
2024-08-19 18:56:39 -0300

    created the billboard folder with its class and genre enumerator

---
+++ entities_library/billboard/Billboard.cs
@@ -0,0 +1,10 @@
+using entitities_library.post;
+
+namespace entities_library.billboard;
+
+public class Billboard
+{
+    public required string Name { get; set; }
+
+    public DateTime PublicationDate{ get; set; }
+}
---
+++ entities_library/billboard/Genre.cs
@@ -0,0 +1,17 @@
+namespace entities_library.billboard;
+
+public enum ReportStatus
+{
+    NewRelases,
+    Pop,
+    Rock,
+    Jazz,
+    Country,
+    Classic,
+    Metal,
+    HipHop,
+    CUarteto,
+    Reguee,
+    Regueton
+
+}
