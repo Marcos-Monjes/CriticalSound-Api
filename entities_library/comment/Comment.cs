6e70315618c9ddd673ae8b5cf2e2b6ca18356019
mcarr128 <167709819+mcarr128@users.noreply.github.com>
2024-08-19 12:16:46 -0300

    comment class added

---
+++ entities_library/comment/Comment.cs
@@ -0,0 +1,15 @@
+namespace entitities_library.comment;
+
+public class Comment
+{
+    public long Id { get; set; }
+
+    public required string Text { get; set; }
+
+    public required User User { get; set; } 
+
+    public DateTime Date { get; set; }  
+
+    public ReactionType ReactionType { get; set; }  
+
+}
