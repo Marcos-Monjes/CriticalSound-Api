708db4e0016ef4c220af90491f1a144eb58d4367
Marcos Monjes <marcos.monjes@itscordoba.edu.ar>
2024-09-30 21:41:48 -0300

    The database was updated due to the implementation of the Comment DAO.

---
+++ dao_library/Migrations/20241001003955_ImplementacionDeDAOSComment.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace daolibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImplementacionDeDAOSComment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
