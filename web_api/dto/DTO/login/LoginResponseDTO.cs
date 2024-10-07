5f19e6af940f74f0bbc11b7dba07436006914ce3
Santiago Carranza <carranzasantiago85@gmail.com>
2024-10-06 21:04:03 -0300

    name and lastname were removed since they are not necessary for the front

--- web_api/dto/DTO/login/LoginResponseDTO.cs
+++ web_api/dto/DTO/login/LoginResponseDTO.cs
@@ -5,8 +5,7 @@ namespace web_api.dto.login;
 public class LoginResponseDTO : ResponseDTO
 {
     public long id { get; set; }
-    public string name { get; set; } = "";
-    public string lastName { get; set; } = "";
+    public string userName { get; set; } = "";
     public string description { get; set; } = "";
     public string urlAvatar { get; set; } = "";
     public string mail { get; set; } = "";
