149264419610e9e474528cd5f9c4cd28d033247e
Santiago Carranza <carranzasantiago85@gmail.com>
2024-10-11 20:44:04 -0300

    add migrations

---
+++ dao_library/Migrations/20241007000823_DaosInyeccionRegister.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace daolibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class DaosInyeccionRegister : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Persons");
+
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "Persons",
+                newName: "password");
+
+            migrationBuilder.AddColumn<string>(
+                name: "userName",
+                table: "Persons",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "userName",
+                table: "Persons");
+
+            migrationBuilder.RenameColumn(
+                name: "password",
+                table: "Persons",
+                newName: "Password");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Persons",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Persons",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
