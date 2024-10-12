4061c98aa79b96c6bad0b9bb7dca720f5a68bc30
Santiago Carranza <carranzasantiago85@gmail.com>
2024-10-12 20:36:15 -0300

    remove unseless code

--- entities_library/login/Person.cs
+++ entities_library/login/Person.cs
@@ -10,21 +10,3 @@ public class Person
 }
 
 
-
-
-// namespace entities_library.login;
-
-// public class Person 
-// {
-//     public long Id { get; set; }
-
-// eliminar estos datos en caso que no esten en el formualrio de registro
-//     public required string Name { get; set; } 
-
-//     public required string Username{ get; set; }
-//     public required string LastName {get; set;}
-
-//     public required string Mail {get; set;}
-
-//     public DateTime? Birthdate { get; set; }
-// }
