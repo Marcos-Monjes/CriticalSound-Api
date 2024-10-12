81017bd74f6df024bd80a93a2882837268592e22
Santiago Carranza <carranzasantiago85@gmail.com>
2024-10-12 20:34:30 -0300

    add changes on database

---
+++ dao_library/Migrations/20241012161940_DaoGenreInyeccionDependencia.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace daolibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class DaoGenreInyeccionDependencia : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "genretype",
+                table: "Genres",
+                newName: "genreType");
+
+            migrationBuilder.AddColumn<string>(
+                name: "description",
+                table: "Genres",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "description",
+                table: "Genres");
+
+            migrationBuilder.RenameColumn(
+                name: "genreType",
+                table: "Genres",
+                newName: "genretype");
+        }
+    }
+}
